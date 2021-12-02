d2c97be88d2011ac203ca703941e8b7ba7e68ccc
可可同学 <AndroidLeo@outlook.com>
2021-12-02 16:27:08 +0800

    --

---
+++ js/config.js
@@ -0,0 +1,5 @@
+var config = {
+  key: '22c50e1b0516183debadb8af53014208'
+}
+
+module.exports.Config = config;
---
+++ xinmi/details/service/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: 可可同学
+ * @Date: 2021-12-02 16:14:31
+ * @LastEditTime: 2021-12-02 16:19:43
+ * @LastEditors: 可可同学
+ * @Description: 
+ */
+require("./index.service");
