52baf414f950d985817287da1b93f2c82b0c62a8
可可同学 <AndroidLeo@outlook.com>
2021-12-07 14:29:48 +0800

    --

--- static/api/kanjia.js
+++
@@ -1,9 +0,0 @@
-import request from '@/static/common/request'
-
-export function kanjiaList(data) {
-  return request.post('/shop/goods/kanjia/list', data)
-}
-
-export function kanjiaSet(data) {
-  return request.get('/shop/goods/kanjia/set', data)
-}
